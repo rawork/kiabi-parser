24f3f7c0def93991ded705ac80aefbfd7a38738d
Roman Alyakritskiy <rawork@yandex.ru>
2017-03-16 17:41:01 +0300

    fixes

--- src/Kiabi/YandexParser.php
+++ src/Kiabi/YandexParser.php
@@ -72,11 +72,15 @@ class YandexParser
 		}
 
 		$content .= '	</categories>
-	<delivery-options>
-		<option cost="0" days="1-2" order-before="24"/>
-	</delivery-options>
 	<offers>
 ';
+
+		/*
+		 * <delivery-options>
+		<option cost="0" days="1-2" order-before="24"/>
+	</delivery-options>
+		 *
+		 */
 
 		return $content;
 	}
