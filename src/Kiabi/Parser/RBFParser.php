0b23ce094a8d2b39d089eca24417c65806e32df3
Roman Alyakritskiy <rawork@yandex.ru>
2017-11-12 20:35:11 +0300

    fixes

--- src/Kiabi/Parser/RBFParser.php
+++ src/Kiabi/Parser/RBFParser.php
@@ -302,15 +302,16 @@ class RBFParser
 
                 $rbfGood = $rbfGoods[md5($reference['link_coloris_https'][0])];
                 $this->r++;
-				echo 'Found', '['.$this->r.'] '.$rbfGood['Link']."\n";
-                echo 'RBF Price = '.$sku['price'][0] / 100 * intval(str_replace('%', '', $rbfGood['Discount']))."\n";
-                echo 'Feed sale price = '.$sku['sale_price'][0]."\n";
 
                 $oldprice = '
             <oldprice>'.$sku['price'][0].'</oldprice>
 ';
                 $price = $sku['sale_price'][0];
-                $price = number_format($sku['price'][0] / 100 * intval(str_replace('%', '', $rbfGood['Discount'])), 2, '.');
+                $price = number_format($sku['price'][0] / 100 * intval(str_replace('%', '', $rbfGood['Discount'])), 2, '.', '');
+
+                echo 'Found', '['.$this->r.'] '.$rbfGood['Link']."\n";
+                echo 'RBF Price = '.$price."\n";
+                echo 'Feed sale price = '.$sku['sale_price'][0]."\n";
 
 				$content .= '<offer id="'.$sku['code'][0].'" available="'.$available.'">
                 <url>'.$reference['link_coloris_https'][0].($this->addUtmMark ? $this->utmMark : '').'</url>
