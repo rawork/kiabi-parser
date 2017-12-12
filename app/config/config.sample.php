d1fe09e4dd3f845f2ecd0b88b09a97a10f25d815
Roman Alyakritskiy <rawork@yandex.ru>
2017-12-12 13:57:47 +0300

    fixes

--- app/config/config.sample.php
+++ app/config/config.sample.php
@@ -2,14 +2,13 @@
 
 define('PRJ_DIR', __DIR__.'/../../');
 
-define('MAINPAGE_URL', 'http://www.kiabi.ru/');
-define('FEED_URL', 'http://www.kiabi.com/googleshopping_RU_ru.xml');
+define('STORE_TITLE', 'Kiabi');
+define('COMPANY_TITLE', 'Kiabi Europe SAS');
+define('STORE_URL', 'https://www.kiabi.ru');
+define('FEED_URL', 'https://www.kiabi.com/googleshopping_RU_ru.xml');
 define('TIMEFILE_PATH', __DIR__.'/../../files/time.txt');
-define('MAINPAGE_PATH', __DIR__.'/../../files/mainpage.html');
-define('CATEGORIES_PATH', __DIR__.'/../../files/categories.xml');
 define('YANDEX_CATEGORIES_PATH', __DIR__.'/../../files/categories_yandex.json');
 define('YANDEX_COLORS_PATH', __DIR__.'/../../files/colors_yandex.json');
-define('CP_CATEGORIES_PATH', __DIR__.'/../../files/channel_pilot_categories.xml');
 
 define('RBF_GOODS_PATH', __DIR__.'/../../files/rbf_goods.json');
 define('RBF_GOODS_RAW_PATH', __DIR__.'/../../files/rbf_goods_raw.json');
@@ -18,7 +17,7 @@ define('GOOGLE_CATEGORIES_JSON_PATH', __DIR__.'/../../files/categories_google.js
 define('GOOGLE_CATEGORIES_XSLX_PATH', __DIR__.'/../../files/categories_google.xlsx');
 define('GOOGLE_TAXONOMY_PATH', __DIR__.'/../../public/files/taxonomy_google.xls');
 
-define('FEED_ORIGINAL_PATH', __DIR__.'/../../files/feed_original_for_yandex.xml');
+define('FEED_ORIGINAL_PATH', __DIR__.'/../../files/feed_original.xml');
 
 define('FEED_CONVERTED_YANDEX_PATH', __DIR__.'/../../files/feed_converted_yandex.xml');
 define('FEED_CONVERTED_RBF_PATH', __DIR__.'/../../files/feed_converted_rbf.xml');
@@ -27,6 +26,7 @@ define('FEED_CONVERTED_YANDEX0_PATH', __DIR__.'/../../files/feed_converted_yande
 define('FEED_CONVERTED_YANDEX0_MAIL_PATH', __DIR__.'/../../files/feed_converted_yandex0_mail.xml');
 define('FEED_CONVERTED_YANDEX_SMARTBANNER_PATH', __DIR__.'/../../files/feed_converted_yandex_smartbanner.xml');
 define('FEED_CONVERTED_YANDEX_REMARKETING_PATH', __DIR__.'/../../files/feed_converted_yandex_remarketing.xml');
+define('FEED_CONVERTED_YANDEX_VK_PATH', __DIR__.'/../../files/feed_converted_yandex_vk.xml');
 define('FEED_CONVERTED_AVITO_PATH', __DIR__.'/../../files/feed_converted_avito.xml');
 define('FEED_CONVERTED_PRICE_PATH', __DIR__.'/../../files/feed_converted_price.xml');
 define('FEED_CONVERTED_PUSH4SITE_PATH', __DIR__.'/../../files/feed_converted_push4site.xml');
@@ -35,10 +35,6 @@ define('FEED_CONVERTED_GOOGLE_PATH', __DIR__.'/../../files/feed_converted_google
 define('FEED_CONVERTED_GOOGLE2_PATH', __DIR__.'/../../files/feed_converted_google2.xml');
 define('FEED_CONVERTED_CRITEO_PATH', __DIR__.'/../../files/feed_converted_criteo.xml');
 define('FEED_CONVERTED_FACEBOOK_PATH', __DIR__.'/../../files/feed_converted_facebook.xml');
-
-define('STORE_TITLE', 'Kiabi');
-define('COMPANY_TITLE', 'Kiabi Europe SAS');
-define('STORE_URL', 'https://www.kiabi.ru');
 
 define('LINK_COUNTER_APPENDIX_AVITO', '#'.htmlspecialchars('&utm_source=avito&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={id}_{platform}_{pos}&ns_mchannel=display&ns_source=avito&ns_campaign=pma5'));
 define('LINK_COUNTER_APPENDIX_PRICE', '#'.htmlspecialchars('#&ns_mchannel=cpc&ns_source=priceru&ns_campaign={campaign_id}&utm_medium=cpc&utm_source=priceru&utm_campaign={campaign_id}'));
@@ -50,15 +46,8 @@ define('LINK_COUNTER_APPENDIX_YANDEX', '#'.htmlspecialchars('&utm_source=YandexM
 define('LINK_COUNTER_APPENDIX_RBF', '');
 define('LINK_COUNTER_APPENDIX_YANDEX_SMARTBANNER', '#'.htmlspecialchars('&utm_source=yandex&utm_medium=cpc&utm_campaign=Smart_Banner&utm_content={ad_id}&utm_term={keyword}_{source}&ns_mchannel=cpc&ns_source=yandex&ns_campaign=hbrand'));
 define('LINK_COUNTER_APPENDIX_YANDEX_REMARKETING', '#'.htmlspecialchars('&utm_source=yandex&utm_medium=cpc&utm_campaign=Din_remarketing&utm_content={ad_id}&utm_term={adtarget_id}_{adtarget_name}&ns_mchannel=cpc&ns_source=yandex&ns_campaign=hbrand'));
-
+define('LINK_COUNTER_APPENDIX_YANDEX_VK', '?utm_source=vk&amp;utm_medium=retargeting&amp;utm_campaign={campaign_id}&amp;utm_content={offer_id}#&amp;ad_id={ad_id}&amp;client_id={client_id}&amp;price_list_id={price_list_id}&amp;product_id={product_id}&amp;platform={platform}');
 define('LINK_COUNTER_APPENDIX_YANDEX0_MAIL', '?#'.htmlspecialchars('&utm_source=mytarget&utm_medium=cpc&utm_campaign=dinremarketing&utm_content={{campaign_name}}'));
-
-/*
- * todo try use rawurlencode () for utm
- */
-
-
-
 
 define('DB_HOST', 'localhost');
 define('DB_BASE', '');
