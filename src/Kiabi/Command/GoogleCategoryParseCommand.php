496d4c254fa3c3f28ff0e0c43017ce4b71434e2e
Roman Alyakritskiy <rawork@yandex.ru>
2018-03-22 20:25:44 +0300

    fixes

--- src/Kiabi/Command/GoogleCategoryParseCommand.php
+++ src/Kiabi/Command/GoogleCategoryParseCommand.php
@@ -5,6 +5,7 @@ namespace Kiabi\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\NullOutput;
 
 class GoogleCategoryParseCommand extends Command
