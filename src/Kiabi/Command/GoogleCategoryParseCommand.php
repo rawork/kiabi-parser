e69fcb3dd8abd742b715ce9173e8c8c69526adad
Roman Alyakritskiy <rawork@yandex.ru>
2018-03-29 23:15:09 +0300

    fixes

--- src/Kiabi/Command/GoogleCategoryParseCommand.php
+++ src/Kiabi/Command/GoogleCategoryParseCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\NullOutput;
+use PHPExcel_Style_Alignment;
 
 class GoogleCategoryParseCommand extends Command
 {
